d00aa02040a6bb9cccfeb32793b71f842b9bc0f7
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-05-16 23:20:15 -0300

    Projects att

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,5 +1,6 @@
 import "./Tabs.css";
 import GetAPetImage from '../Assets/projects/GetAPet.gif';
+import GaPLogo from '../Assets/logos/GetaPetProjectLogo.png';
 import { useState } from 'react'
 
 function Tabs() {
@@ -24,22 +25,22 @@ function Tabs() {
     <div className="TabsContainer">
         <div className="bloc-tabs">
             <button
-             className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
+             className={toggleState === 1 ? "tabs active-tabs btnActive" : "tabs btnDesativeted"}
              onClick={() =>toggleTab(1)}
              >Todos</button>
 
             <button
-             className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
+             className={toggleState === 2 ? "tabs active-tabs btnActive" : "tabs btnDesativeted"}
              onClick={() =>toggleTab(2)}
             >Frontend</button>
 
             <button
-             className={toggleState === 3 ? "tabs active-tabs" : "tabs"}
+             className={toggleState === 3 ? "tabs active-tabs btnActive" : "tabs btnDesativeted"}
              onClick={() =>toggleTab(3)}
             >Backend</button>
 
             <button
-             className={toggleState === 4 ? "tabs active-tabs" : "tabs"}
+             className={toggleState === 4 ? "tabs active-tabs btnActive" : "tabs btnDesativeted"}
              onClick={() =>toggleTab(4)}
             >Jogos</button>
         </div>
@@ -53,7 +54,9 @@ function Tabs() {
                     <div className="ImageProject">
                         <img src={GetAPetImage}></img>
                     </div>
-                    <h2>Logo</h2>
+                    <div className="logo">
+                        <img src={GaPLogo}></img>
+                    </div>
                     <p>Um site que simula uma adoção completa de pet, contendo regras de negocios, alem de um sistema que checagem.</p>
                     <h3>Technologies:</h3>
                     <div className="technologies">
@@ -63,18 +66,48 @@ function Tabs() {
                         <button>Visit Site</button>
                         <button>See source code</button>
                     </div>
-                </section> 
-
-                
-                
-              
+                </section>    
             </div>
 
             <div
              className={toggleState === 2 ? "content active-content" : "content"}>
-                <section>
-                    <h2>IMG</h2>
-                    <p>Frotend</p>
+
+                <section className="sectionProject">
+
+                    <div className="ImageProject">
+                        <img src={GetAPetImage}></img>
+                    </div>
+                    <div className="logo">
+                        <img src={GaPLogo}></img>
+                    </div>
+                    <p>Um site que simula uma adoção completa de pet, contendo regras de negocios, alem de um sistema que checagem.</p>
+                    <h3>Technologies:</h3>
+                    <div className="technologies">
+                        <h4>Html</h4><h4>Css</h4><h4>Javascript</h4><h4>Nodejs</h4><h4>Reactjs</h4><h4>Sequelize</h4>
+                    </div>
+                    <div className="btnSection">
+                        <button>Visit Site</button>
+                        <button>See source code</button>
+                    </div>
+                </section>  
+
+                <section className="sectionProject">
+
+                    <div className="ImageProject">
+                        <img src={GetAPetImage}></img>
+                    </div>
+                    <div className="logo">
+                        <img src={GaPLogo}></img>
+                    </div>
+                    <p>Um site que simula uma adoção completa de pet, contendo regras de negocios, alem de um sistema que checagem.</p>
+                    <h3>Technologies:</h3>
+                    <div className="technologies">
+                        <h4>Html</h4><h4>Css</h4><h4>Javascript</h4><h4>Nodejs</h4><h4>Reactjs</h4><h4>Sequelize</h4>
+                    </div>
+                    <div className="btnSection">
+                        <button>Visit Site</button>
+                        <button>See source code</button>
+                    </div>
                 </section> 
             </div>
 
